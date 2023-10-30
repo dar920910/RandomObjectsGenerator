8e85777e127e7b767b19df0ab08a5d1d69adcf94
Denis Rezepov <me@dar920910.ru>
2023-10-30 16:27:39 +0300

    Prepared an initial console app.

---
+++ RandomObjectsGenerator.App.CLI/src/Program.cs
@@ -0,0 +1,35 @@
+﻿//---------------------------------------------------------------------------
+// <copyright file="Program.cs" company="Demo Projects Workshop">
+// Copyright (c) Demo Projects Workshop. All rights reserved.
+// Licensed under the MIT license. See LICENSE.md file in the project root for full license information.
+// </copyright>
+// ---------------------------------------------------------------------------
+
+using RandomObjectsGenerator.Library.Core;
+using RandomObjectsGenerator.Library.TargetModels;
+using static System.Console;
+
+const int targetCountOfObjects = 10;
+
+// Step # 1.
+WriteLine($"Creating a collection from {targetCountOfObjects} randomly generated Person objects in memory ...");
+Person[] persons = PersonGenerator.GenerateTestObjects(targetCountOfObjects);
+
+// Step # 2.
+WriteLine("Serializing the collection to the JSON format ...");
+
+// Step # 3.
+WriteLine("Writing the serialization result to the current user desktop directory into the 'Persons.json' text file ...");
+
+// Step # 4.
+WriteLine("Clearing the collection from the database in-memory ...");
+
+// Step # 5.
+WriteLine("Reading serialized objects from the 'Persons.json' text file");
+
+// Step # 6.
+WriteLine("Printing persons' count, credit card count, and the average values of child age ...");
+foreach (Person person in persons)
+{
+    WriteLine($"Person {person.FirstName} {person.LastName} has {person.CreditCardNumbers.Length}.");
+}
---
+++ RandomObjectsGenerator.Library.Core/src/PersonGenerator.cs
@@ -0,0 +1,50 @@
+﻿//---------------------------------------------------------------------------
+// <copyright file="PersonGenerator.cs" company="Demo Projects Workshop">
+// Copyright (c) Demo Projects Workshop. All rights reserved.
+// Licensed under the MIT license. See LICENSE.md file in the project root for full license information.
+// </copyright>
+// ---------------------------------------------------------------------------
+
+namespace RandomObjectsGenerator.Library.Core;
+
+using RandomObjectsGenerator.Library.TargetModels;
+
+/// <summary>
+/// Implements the generator for objects of the Person class.
+/// </summary>
+public static class PersonGenerator
+{
+    /// <summary>
+    /// Generates test Person objects in the specified count (only for testing aims).
+    /// </summary>
+    /// <param name="count">The target count of instances of the object to creation.</param>
+    /// <returns>The array of the Person objects.</returns>
+    public static Person[] GenerateTestObjects(uint count)
+    {
+        Person person = new ()
+        {
+            Id = 0,
+            SequenceId = 0,
+            TransportId = Guid.NewGuid(),
+            FirstName = "Person's First Name",
+            LastName = "Person's Last Name",
+            Gender = Gender.Male,
+            Age = 100,
+            BirthDate = 100_000,
+            CreditCardNumbers = Array.Empty<string>(),
+            Phones = Array.Empty<string>(),
+            Salary = 100_000M,
+            IsMarried = false,
+            Children = Array.Empty<Child>(),
+        };
+
+        List<Person> persons = new ();
+
+        for (int i = 0; i < 5; i++)
+        {
+            persons.Add(person);
+        }
+
+        return persons.ToArray();
+    }
+}
