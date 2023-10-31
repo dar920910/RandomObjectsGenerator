fdb1601949be10ba585bbe8573b3e9aeb17baf51
Denis Rezepov <me@dar920910.ru>
2023-10-31 16:01:30 +0300

    Implemented serialization operations for the collection of the Person objects.

--- RandomObjectsGenerator.App.CLI/src/Program.cs
+++ RandomObjectsGenerator.App.CLI/src/Program.cs
@@ -7,16 +7,14 @@
 
 using RandomObjectsGenerator.Library.Core;
 using RandomObjectsGenerator.Library.DatabaseContext.InMemory;
+using RandomObjectsGenerator.Library.Serialization;
 using RandomObjectsGenerator.Library.TargetModels;
 using static System.Console;
 
-const int targetCountOfObjects = 10;
-const string targetOutputFileName = "Persons.txt";
+const int targetCountOfObjects = 100;
 
+WriteLine($"Creating a collection from {targetCountOfObjects} randomly generated Person objects in memory ...");
 DatabaseContext databaseInMemory = new ();
-
-WriteLine($"Step # 1: Creating a collection from {targetCountOfObjects} randomly generated Person objects in memory ...");
-
 for (int personIndex = 0; personIndex < targetCountOfObjects; personIndex++)
 {
     Person person = PersonGenerator.CreateRandomPerson(personIndex);
@@ -26,29 +24,11 @@ for (int personIndex = 0; personIndex < targetCountOfObjects; personIndex++)
 databaseInMemory.SaveChanges();
 WriteLine();
 
-WriteLine("Step # 2: Serializing the collection to the JSON format ...");
-
+WriteLine("Serializing the collection to the JSON format ...");
+SerializationManager.SaveToJsonTextFile(databaseInMemory.Persons.ToList());
 WriteLine();
 
-WriteLine($"Step # 3: Writing the serialization result to the current user desktop directory into the '{targetOutputFileName}' text file ...");
-
-string outputFolderPath = (Environment.OSVersion.Platform == PlatformID.Win32NT) ?
-    Environment.GetFolderPath(Environment.SpecialFolder.Desktop) : Environment.CurrentDirectory;
-WriteLine($"Output Folder Path: '{outputFolderPath}'");
-string outputFilePath = Path.Combine(outputFolderPath, targetOutputFileName);
-
-StreamWriter stream = new (outputFilePath);
-
-foreach (Person person in databaseInMemory.Persons)
-{
-    stream.WriteLine($"Person {person.Id}: '{person.FirstName} {person.LastName}'");
-}
-
-stream.Close();
-WriteLine();
-
-WriteLine("Step # 4: Clearing the collection from the database in-memory ...");
-
+WriteLine("Clearing the collection from the database in-memory ...");
 foreach (var person in databaseInMemory.Persons)
 {
     databaseInMemory.Persons.Remove(person);
@@ -57,21 +37,19 @@ foreach (var person in databaseInMemory.Persons)
 databaseInMemory.SaveChanges();
 WriteLine();
 
-WriteLine("Step # 5: Reading serialized objects from the 'Persons.json' text file");
-
-string[] personRecords = File.ReadAllLines(outputFilePath);
-WriteLine();
-
-WriteLine("Step # 6: Printing persons' count, credit card count, and the average values of child age ...");
-
-foreach (Person person in databaseInMemory.Persons)
+WriteLine("Reading serialized objects from the JSON text file");
+List<Person> persons = SerializationManager.ReadFromJsonTextFile();
+foreach (Person person in persons)
 {
-    WriteLine($"Person: {person.Id} {person.FirstName} {person.LastName}");
+    WriteLine($"Person {person.Id}: '{person.FirstName} {person.LastName}'");
 }
 
-foreach (string personRecord in personRecords)
+WriteLine();
+
+WriteLine("Printing persons' count, credit card count, and the average values of child age ...");
+foreach (Person person in persons)
 {
-    WriteLine(personRecord);
+    WriteLine($"Person: {person.Id} {person.FirstName} {person.LastName}");
 }
 
 WriteLine();
--- RandomObjectsGenerator.Library.Serialization/src/SerializationManager.cs
+++ RandomObjectsGenerator.Library.Serialization/src/SerializationManager.cs
@@ -7,6 +7,8 @@
 
 namespace RandomObjectsGenerator.Library.Serialization;
 
+using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
 using RandomObjectsGenerator.Library.TargetModels;
 
 /// <summary>
@@ -14,4 +16,60 @@ using RandomObjectsGenerator.Library.TargetModels;
 /// </summary>
 public static class SerializationManager
 {
+    private const string OutputPersonsFileName = "Persons.json";
+
+    /// <summary>
+    /// Saves the specified Person objects to the JSON text file.
+    /// </summary>
+    /// <returns>The list of objects which are read as instances of the Person class.</returns>
+    public static List<Person> ReadFromJsonTextFile()
+    {
+        string outputFilePath = GetOutputPersonsFilePath();
+
+        List<Person> persons = new ();
+
+        using (StreamReader stream = File.OpenText(outputFilePath))
+        {
+            JsonSerializer serializer = new ();
+            persons = (List<Person>)serializer.Deserialize(stream, typeof(List<Person>));
+        }
+
+        return persons;
+    }
+
+    /// <summary>
+    /// Saves the specified Person objects to the JSON text file.
+    /// </summary>
+    /// <param name="collection">The list of objects which are instances of the Person class.</param>
+    public static void SaveToJsonTextFile(List<Person> collection)
+    {
+        string outputFilePath = GetOutputPersonsFilePath();
+
+        using (StreamWriter stream = File.CreateText(outputFilePath))
+        {
+            DefaultContractResolver contractResolver = new ()
+            {
+                NamingStrategy = new CamelCaseNamingStrategy()
+                {
+                    OverrideSpecifiedNames = false,
+                },
+            };
+
+            string json = JsonConvert.SerializeObject(collection, new JsonSerializerSettings()
+            {
+                ContractResolver = contractResolver,
+                Formatting = Formatting.Indented,
+            });
+
+            stream.WriteLine(json);
+        }
+    }
+
+    private static string GetOutputPersonsFilePath()
+    {
+        string outputFolderPath = (Environment.OSVersion.Platform == PlatformID.Win32NT) ?
+            Environment.GetFolderPath(Environment.SpecialFolder.Desktop) : Environment.CurrentDirectory;
+
+        return Path.Combine(outputFolderPath, OutputPersonsFileName);
+    }
 }
